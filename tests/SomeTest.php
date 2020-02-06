9b1ddc872a46f81bac0208be15582a456b10fe8d
steph bug <plexikon@protonmail.com>
2020-02-06 12:14:47 +0100

    first commit

---
+++ tests/SomeTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Plexikon\Moc\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class SomeTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_assert_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
